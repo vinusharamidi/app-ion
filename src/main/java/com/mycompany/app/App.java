8776e09da47c0df5e555068e252e62f5610a308b
SARATH RACHERLA <sharathsaee@gmail.com>
2020-03-06 07:22:13 +0530

    721-06032020-Changed txt

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Changes brough by JENKINS" );
+        System.out.println( "Hello World" );
     }
 }
